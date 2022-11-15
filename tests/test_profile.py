ed4b2882bc487c3d798d19e36bde600b33821b0f
eugeny.okulik <tiburon854@gmail.com>
2022-11-15 21:56:07 +0300

    Добавил примеры как разделять тесты

---
+++ tests/test_profile.py
@@ -0,0 +1,14 @@
+class TestUserDetails:
+    def test_name(self):
+        pass
+
+    def test_surname(self):
+        pass
+
+
+class TestSocial:
+    def test_instagram(self):
+        pass
+
+    def test_twitter(self):
+        pass
